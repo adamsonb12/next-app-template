4a2f859f301fc302b627761d617ef45c566090de
Jason Hamm <jasonhamm.me@gmail.com>
2023-06-19 10:50:01 -0600

    Create route to create mocks

---
+++ src/app/create-mocks/route.tsx
@@ -0,0 +1,9 @@
+import { prisma } from "@/server/prisma-client";
+import { parse } from "date-fns";
+
+export async function GET() {
+
+  // TODO - add mocks 
+
+  return new Response("Create Mocks");
+}
