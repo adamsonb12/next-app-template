72a85ce36397429519ed7d85a181f846c963083f
Jason Hamm <jasonhamm.me@gmail.com>
2023-07-18 15:39:04 -0600

    Move ApolloWrapper

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,12 +4,15 @@ import { ReactNode } from "react";
 import { ThemeProvider } from "styled-components";
 import { StyledComponentsRegistry } from "@/lib/styled-component-registry";
 import { theme } from "@/providers/theme";
+import { ApolloWrapper } from "@/lib/apollo-provider";
 
 export const ClientProviders = ({ children }: { children: ReactNode }) => {
   return (
     <>
       <StyledComponentsRegistry>
-        <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        <ThemeProvider theme={theme}>
+          <ApolloWrapper>{children}</ApolloWrapper>
+        </ThemeProvider>
       </StyledComponentsRegistry>
     </>
   );
